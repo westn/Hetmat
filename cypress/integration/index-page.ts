f422c94f56fcb48d88084205ea055960f7ec0d34
Fredrik Nordell <fredrik.nordell@sigma.se>
2020-12-05 11:14:22 +0100

    add accessibility testing

--- cypress/integration/index-page.ts
+++ cypress/integration/index-page.ts
@@ -7,6 +7,11 @@ describe("Index page", () => {
   it("should have a title", () => {
     cy.get('[data-test="title"]').should("have.text", "Recepttitle");
   });
+
+  it("should be accessible", () => {
+    cy.injectAxe();
+    cy.checkA11y();
+  });
 });
 
 export {};
