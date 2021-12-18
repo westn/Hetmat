93674084d4684979a4aa9bff187d417a14ec008e
Fredrik Nordell <fredrik.nordell@sigma.se>
2021-12-18 14:16:34 +0100

    fix tailwind and nextjs configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
-    extend: {
-      colors: {
-        blue: {
-          500: "rgb(37 19 187)",
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
